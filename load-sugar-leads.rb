8673436384292baef6fb659abc453be8a1181ef1
Jason Goecke <jsgoecke@mayluc.local>
2008-11-28 13:16:04 -0800

    Changes to ensure we may enter leads appropriately

--- load-sugar-leads.rb
+++ load-sugar-leads.rb
@@ -3,12 +3,13 @@ require 'rubygems'
 require 'configatron'
 require 'soap/wsdlDriver'
 require 'digest/md5'
- 
-puts ''
-puts ''
-puts 'Starting to load the leads'
+
+puts 'Welcome to the load-sugar-leads utility'
+
 #Load our configuration file
+puts 'Loading configuration file...'
 $config = configatron.configure_from_yaml(File.expand_path(File.dirname(__FILE__) + "/config/config.yml"))
+puts 'Configuration file loaded...'
 
 #Build a hash of the CSV file of the data to be imported
 def build_csv_data 
@@ -36,7 +37,7 @@ end
 
 #Build the address field with multiple lines
 def build_address lead
-  fields = $config["field_associations"]["primary_address"].split(',')
+  fields = $config["field_associations"]["primary_address_street"].split(',')
   address = String.new
   fields.each do |field|
     if lead[field] != ""
@@ -63,22 +64,25 @@ def build_lead lead
   address = build_address(lead)
   description = build_description(lead)
   built_lead = [
-                  { :name => "lead_status", :value => $config["global_settings"]["lead_status"] },
+                  { :name => "status", :value => $config["global_settings"]["status"] },
+                  { :name => "status_description", :value => $config["global_settings"]["status_description"] },
                   { :name => "lead_source", :value => $config["global_settings"]["lead_source"] },
                   { :name => "lead_source_description", :value => $config["global_settings"]["lead_source_description"] },
-                  { :name => "assigned_to", :value => $config["global_settings"]["assigned_to"] },
+                  { :name => "refered_by", :value => $config["global_settings"]["refered_by"] },
+                  { :name => "assigned_user_name", :value => $config["global_settings"]["assigned_user_name"] },
+                  { :name => "assigned_user_id", :value => $config["global_settings"]["assigned_user_id"] },
                   { :name => "first_name", :value => lead[$config["field_associations"]["first_name"]] },
                   { :name => "last_name", :value => lead[$config["field_associations"]["last_name"]] },
                   { :name => "account_name", :value  => lead[$config["field_associations"]["account_name"]] },
                   { :name => "title", :value => lead[$config["field_associations"]["title"]] },
-                  { :name => "office_phone", :value => lead[$config["field_associations"]["office_phone"]] },
+                  { :name => "phone_work", :value => lead[$config["field_associations"]["phone_work"]] },
                   { :name => "fax", :value => lead[$config["field_associations"]["fax"]] },
-                  { :name => "email", :value => lead[$config["field_associations"]["email"]] },
-                  { :name => "primary_addresss", :value => address },
-                  { :name => "city", :value => lead[$config["field_associations"]["city"]] },
-                  { :name => "state", :value => lead[$config["field_associations"]["state"]] },
+                  { :name => "email1", :value => lead[$config["field_associations"]["email1"]] },
+                  { :name => "primary_address_street", :value => address },
+                  { :name => "primary_address_city", :value => lead[$config["field_associations"]["primary_address_city"]] },
+                  { :name => "primary_address_state", :value => lead[$config["field_associations"]["primary_address_state"]] },
                   { :name => "postal_code", :value => lead[$config["field_associations"]["postal_code"]] },
-                  { :name => "country", :value => lead[$config["field_associations"]["country"]] },
+                  { :name => "primary_address_country", :value => lead[$config["field_associations"]["primary_address_country"]] },
                   { :name => "description", :value => description }
                 ]
   return built_lead
@@ -90,7 +94,9 @@ credentials = { "user_name" => $config["global_settings"]["username"],
 
 begin
   #Connect to the Sugar CRM WSDL and build our methods in Ruby
+  puts 'Loading SugarCRM WSDL...'
   ws_proxy = SOAP::WSDLDriverFactory.new($config["global_settings"]["wsdl_url"]).create_rpc_driver
+  puts 'SugarCRM WDSL loaded...'
   ws_proxy.options['protocol.http.ssl_config.verify_mode'] = OpenSSL::SSL::VERIFY_NONE
   
   #This may be toggled on to log XML requests/responses for debugging
@@ -99,6 +105,7 @@ begin
   end
   
   #Login to Sugar CRM
+  puts 'Attempting to login to SugarCRM...'
   session = ws_proxy.login(credentials, nil)
 rescue => err
   puts err
@@ -111,23 +118,24 @@ if session.error.number.to_i != 0
   puts "Exiting"
   abort
 else
-  puts "Successfully logged in"
+  puts "Successfully logged in..."
 end
 
+puts 'Scanning the CSV file...'
 csv_leads = build_csv_data
 
 cnt = 0
+puts 'Beginning to the process of inserting leads into Sugar via SOAP...'
 csv_leads.each do |lead|
   built_lead = build_lead(lead)
-  if cnt == 0
-    results = ws_proxy.set_entry(session['id'], $config["global_settings"]["module"], built_lead)
-    if session.error.number.to_i != 0
-      puts session.error.description + " (" + session.error.number + ")"
-    else
-      puts 'Successfully processed record # ' + (cnt + 1).to_s
-    end
+  results = ws_proxy.set_entry(session['id'], $config["global_settings"]["module"], built_lead)
+  if session.error.number.to_i != 0
+    puts session.error.description + " (" + session.error.number + ")"
+  else
+    puts 'Successfully processed record # ' + (cnt + 1).to_s
   end
   cnt += 1
 end
 
-puts "Processed " + (cnt + 1).to_s + " records"
+puts "Processed " + cnt.to_s + " records..."
+puts "Complete, exiting..."
